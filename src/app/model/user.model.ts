b3f1d17392d8862d0c09c2dcb4bbeab8477e2e74
Michele <micheleierniero@gmail.com>
2023-03-07 15:03:50 +0100

    modifiche model

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -2,29 +2,4 @@ export class User {
     id!: number;
     name!: string;
     age!: number;
-    username!: string;
-    email!: string;
-    address!: Address;
-    phone!: string;
-    website!: string;
-    company!: Company;
-}
-
-export class Address {
-    street!: string;
-    suite!: string;
-    city!: string;
-    zipcode!: string;
-    geo!: Geo;
-}
-
-export class Geo {
-    lat!: string;
-    lng!: string;
-}
-
-export class Company {
-    name!: string;
-    catchPhrase!: string;
-    bs!: string;
 }
